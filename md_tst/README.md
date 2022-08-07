00a399c904f3d17453d39f02afeb37fdded34780
Eddy Soma <esoma92.dev@gmail.com>
2022-08-07 16:27:53 -0400

    add stand reader module

---
+++ md_tst/README.md
@@ -0,0 +1,151 @@
+# JoJo's Stand Table
+
+| Stand | Power | Speed | Range | Persistence | Precision | Potential | Total |
+| --- | --- | --- | --- | --- | --- | --- | --- |
+| Anubis | B | B | E | A | E | C | 18 |
+| Atum | D | C | D | B | D | D | 15 |
+| Bastet | E | E | B | A | E | E | 13 |
+| Cream | B | B | D | C | C | D | 18 |
+| Death Thirteen | C | C | E | B | D | B | 17 |
+| Geb | C | B | A | B | D | D | 20 |
+| High Priestess | C | B | A | A | D | D | 21 |
+| Horus | B | B | D | C | E | C | 17 |
+| Judgement | B | B | C | B | D | D | 19 |
+| Khnum | E | E | E | A | E | E | 10 |
+| Osiris | E | D | D | C | D | D | 12 |
+| Sethan | D | D | E | C | D | D | 12 |
+| Tenore Sax | E | E | D | A | E | E | 11 |
+| Tohth | E | E | E | A | E | E | 10 |
+| Dark Blue Moon | C | C | C | B | C | D | 18 |
+| Emperor | B | B | B | C | E | E | 17 |
+| Ebony Devil | D | D | A | B | D | B | 19 |
+| Empress | C | E | A | A | D | D | 18 |
+| Hanged Man | C | A | A | B | D | D | 21 |
+| Hermit Purple | D | C | D | A | D | E | 15 |
+| Hierophant Green | C | B | A | B | C | D | 21 |
+| Justice | D | E | A | A | E | E | 15 |
+| Lovers | E | D | A | A | D | E | 16 |
+| Magician's Red | B | B | C | B | C | D | 20 |
+| Silver Chariot | C | A | C | B | B | C | 22 |
+| Star Platinum | A | A | C | A | A | A | 28 |
+| Strength | B | D | D | A | E | E | 15 |
+| Sun | B | E | A | A | E | E | 17 |
+| The Fool | B | C | D | C | D | C | 17 |
+| The World | A | A | C | A | B | B | 26 |
+| Tower of Gray | E | A | A | C | E | E | 16 |
+| Wheel of Fortune | B | D | D | A | E | D | 16 |
+| Yellow Temperance | D | C | E | A | E | D | 14 |
+| Achtung Baby | E | E | None | A | E | A | 13 |
+| Aqua Necklace | C | C | A | A | C | E | 20 |
+| Atom Heart Father | E | E | None | A | E | E | 9 |
+| Bad Company | B | B | C | B | C | C | 21 |
+| Boy II Man | C | B | B | A | C | C | 22 |
+| Cheap Trick | E | E | E | A | E | E | 10 |
+| Cinderella | D | C | C | C | A | C | 19 |
+| Crazy Diamond | A | A | D | B | B | C | 23 |
+| Enigma | E | E | C | A | C | C | 16 |
+| Earth Wind and Fire | C | C | None | A | C | C | 17 |
+| Harvest | E | B | A | A | E | C | 19 |
+| Heaven's Door | D | B | B | B | C | A | 22 |
+| Highway Star | C | B | A | A | E | C | 21 |
+| Killer Queen | A | B | D | B | B | A | 24 |
+| Sheer Heart Attack | A | C | A | A | E | A | 24 |
+| Bites the Dust | B | B | A | A | D | A | 25 |
+| Love Deluxe | B | B | C | A | E | B | 21 |
+| Pearl Jam | E | C | B | A | E | C | 17 |
+| Ratt | B | C | D | B | E | C | 17 |
+| Red Hot Chili Pepper | A | A | A | A | C | A | 28 |
+| Super Fly | E | E | None | A | E | E | 9 |
+| Surface | B | B | C | B | C | C | 21 |
+| The Lock | E | E | A | A | E | E | 14 |
+| The Hand | B | B | D | C | C | C | 19 |
+| Echoes (ACT3) | B | B | C | B | C | A | 23 |
+| Stray Cat | B | E | None | A | E | C | 14 |
+| Black Sabbath | A | A | A | A | E | E | 22 |
+| Baby Face | A | B | A | A | E | A | 25 |
+| Gold Experience | C | A | C | D | C | A | 21 |
+| Sticky Fingers | A | A | C | D | C | D | 20 |
+| Metallica | C | C | C | A | C | C | 20 |
+| Purple Haze | A | B | C | E | E | B | 18 |
+| Moody Blues | C | C | A | A | C | C | 22 |
+| Sex Pistols | E | C | B | A | A | B | 22 |
+| The Grateful Dead | B | E | B | A | E | C | 18 |
+| Aerosmith | B | B | B | C | E | C | 19 |
+| Beach Boy | C | B | B | C | C | A | 22 |
+| Man in the Mirror | C | C | B | D | C | E | 16 |
+| Baby Face | A | B | A | A | C | C | 25 |
+| Chariot Requiem | E | E | A | A | E | A | 18 |
+| Clash | D | A | B | A | A | C | 24 |
+| Gold Experience Requiem | Infinity | Infinity | Infinity | Infinity | Infinity | Infinity | 36 |
+| Green Day | A | C | A | A | E | A | 24 |
+| King Crimson | A | A | E | E | B | Infinity | 22 |
+| Kraft Work | A | A | E | C | E | E | 16 |
+| Little Feet | D | B | E | A | D | C | 17 |
+| Mr.President | E | E | E | A | E | E | 10 |
+| Notorious B.I.G | A | Infinity | Infinity | Infinity | E | A | 29 |
+| Oasis | A | A | B | A | E | C | 23 |
+| Rolling Stones | None | B | A | A | E | None | 15 |
+| Soft Machine | A | C | E | A | D | E | 17 |
+| Spice Girl | A | A | C | B | D | C | 22 |
+| Talking Head | E | E | B | A | E | E | 13 |
+| White Album | A | C | C | A | E | E | 18 |
+| Whitesnake | A | D | B | A | C | E | 20 |
+| C-Moon | None | B | B | B | A | D | 19 |
+| Jail House Lock | None | C | B | A | None | None | 12 |
+| Stone Free | A | B | C | A | C | A | 25 |
+| Goo Goo Dolls | D | C | B | D | B | B | 19 |
+| Under World | None | C | A | C | None | None | 11 |
+| Sky High | None | None | B | C | None | None | 7 |
+| Bohemian Rhapsody | None | None | Infinity | A | None | None | 11 |
+| Burning Down the House | None | None | None | None | None | None | 0 |
+| Diver Down | A | A | E | C | B | B | 22 |
+| Dragon's Dream | None | None | None | A | None | None | 5 |
+| Foo Fighters | B | A | C | A | C | B | 24 |
+| Highway to Hell | C | C | A | C | C | C | 20 |
+| Kiss | A | A | A | A | C | A | 28 |
+| Jumpin' Jack Flash | B | C | B | A | D | E | 19 |
+| Limp Bizkit | None | B | B | A | C | E | 17 |
+| Made in Heaven | B | Infinity | C | A | C | A | 26 |
+| Manhattan Transfer | E | E | A | A | A | C | 20 |
+| Marilyn Manson | E | A | A | A | A | C | 24 |
+| Planet Waves | A | B | A | A | E | E | 21 |
+| Survivor | E | E | E | C | E | E | 8 |
+| Weather Report | A | B | C | A | E | A | 23 |
+| Yo-Yo Ma | C | D | A | A | D | C | 20 |
+| 20th Century Boy | None | C | None | A | D | C | 13 |
+| Ball Breaker | A | A | D | B | C | B | 23 |
+| Boku no Rhythm wo Kiitekure | B | C | C | B | E | C | 18 |
+| Catch the Rainbow | C | C | B | B | D | D | 18 |
+| Chocolate Disco | None | C | C | B | A | D | 17 |
+| Civil War | None | C | C | B | C | None | 13 |
+| Cream Starter | D | C | C | A | E | B | 18 |
+| Dirty Deeds Done Dirt Cheap | A | A | C | A | A | A | 28 |
+| D4C Love Train | A | A | C | A | A | C | 26 |
+| Hey Ya! | E | E | E | B | E | E | 9 |
+| In a Silent Way | C | C | D | A | D | B | 19 |
+| Mandom | None | A | None | E | None | C | 9 |
+| Oh! Lonesome Me | E | C | C | B | C | D | 16 |
+| Scary Monsters | B | B | D | A | C | B | 22 |
+| Sugar Mountain | E | E | E | A | E | E | 10 |
+| Tatoo You! | None | E | C | B | E | E | 10 |
+| Ticket to Ride | E | E | E | C | E | C | 10 |
+| Tomb of the Boom 1 2 3 | D | C | C | B | C | C | 18 |
+| Tubular Bells | D | D | D | A | E | B | 16 |
+| Tusk ACT4 | A | B | A | A | B | E | 24 |
+| Wired | D | B | B | B | D | D | 18 |
+| Born This Way | C | B | A | A | E | E | 19 |
+| California King Bed | None | None | E | B | E | E | 7 |
+| Fun Fun Fun | E | C | D | A | E | E | 13 |
+| Nut King Call | C | D | C | A | E | A | 19 |
+| Paisley Park | None | None | A | A | D | C | 15 |
+| Paper Moon King | E | E | C | C | B | E | 13 |
+| Soft & Wet | C | B | D | B | C | A | 21 |
+| Manic Depression | C | A | E | B | B | C | 20 |
+| Voodoo Child | B | A | E | E | B | B | 19 |
+| All Along Watchtower | C | B | A | A | A | E | 23 |
+| Dolly Dagger | A | A | C | A | B | C | 25 |
+| Nightbird Flying | E | A | A | A | E | E | 18 |
+| Purple Haze Distortion | A | B | E | E | C | A | 19 |
+| Rainy Day Dream Away | E | B | A | A | E | E | 17 |
+| Remote Romance | E | D | Infinity | B | C | E | 17 |
+| House of Holy | E | B | C | A | D | D | 17 |
---
+++ src/11_stand_reader.c
@@ -0,0 +1,285 @@
+/*
+ * A program that reads a file containing the data of stands appearing in
+ * "JoJo's Bizarre Adventure" and prints out some aggregate statistics.
+ * Additionally, this data is also written to a markdown file formatted as a
+ * table. The input file is in csv format.
+ *
+ * Concepts:
+ * File I/O.
+ * Using struct to organize data.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_LINE_LEN 128
+#define MAX_STAND_NAME_LEN 40
+#define INITIAL_STAND_ARRAY_CAPACITY 10
+
+const char CSV_DELIMITER[1] = ",";
+const char INPUT_FILE_PATH[] = "./md_tst/stand_input.csv";
+const char OUTPUT_FILE_PATH[] = "./md_tst/README.md";
+const char TEMP_OUTPUT_PATH[] = "./md_tst/stand_output.md.temp";
+
+struct JoJoStand {
+  char name[MAX_STAND_NAME_LEN +
+            1]; /* One extra byte for the null terminator. */
+  int power;
+  int speed;
+  int range;
+  int persistence;
+  int precision;
+  int potential;
+};
+
+/* An unnamed struct to store aggregate statistics. */
+struct {
+  /* Used to calculate averages. */
+  int total_power;
+  int total_speed;
+  int total_range;
+  int total_persistence;
+  int total_precision;
+  int total_potential;
+
+  /* Used to count the occurrences of each stat. */
+  int count_e_or_below;
+  int count_d;
+  int count_c;
+  int count_b;
+  int count_a_or_above;
+} stand_aggregate_stats;
+
+void parse_line_to_stand(struct JoJoStand *stand, char *line);
+int read_stat_text_to_int_and_count(char *stat);
+void process_stand(struct JoJoStand *stand, FILE *output_handle);
+void write_stat_to_table(int stat, FILE *output_handle);
+
+int main(int argc, char const *argv[]) {
+  printf("Reading stand data from <%s>...\n", INPUT_FILE_PATH);
+
+  FILE *input_handle = fopen(INPUT_FILE_PATH, "r");
+  if (input_handle == NULL) {
+    printf("Failed to open the input file.\n");
+    printf("Make sure to execute this in the project's root directory.\n");
+    exit(1);
+  } else {
+    /* Skip the first header line. */
+    char *header_line = malloc(MAX_LINE_LEN + 1);
+    if (header_line == NULL) {
+      printf("Header line allocation failure.\n");
+      exit(1);
+    }
+    fgets(header_line, MAX_LINE_LEN + 1, input_handle);
+    free(header_line);
+  }
+
+  /* Array of stands to hold all data with initial allocation. */
+  int stands_capacity = INITIAL_STAND_ARRAY_CAPACITY;
+  struct JoJoStand *stands = malloc(stands_capacity * sizeof(struct JoJoStand));
+  if (stands == NULL) {
+    printf("Stand array allocation failure.\n");
+    exit(1);
+  }
+
+  int stands_count = 0;
+  char *data_line = malloc(MAX_LINE_LEN + 1);
+  if (data_line == NULL) {
+    printf("Data line allocation failure.\n");
+    exit(1);
+  }
+
+  /* Read the entire file and store stand data into the array. */
+  while (fgets(data_line, MAX_LINE_LEN + 1, input_handle) != NULL) {
+    /* Sanity check, skip empty line(s). */
+    if (strlen(data_line) <= 2) {
+      continue;
+    }
+
+    /* Remove linefeed. */
+    data_line[strcspn(data_line, "\n")] = '\0';
+
+    /* Allocate more memory if needed. */
+    if (stands_count >= stands_capacity) {
+      printf("Read %d rows.\n", stands_count);
+      stands_capacity *= 2;
+      stands = realloc(stands, stands_capacity * sizeof(struct JoJoStand));
+      if (stands == NULL) {
+        printf("Stand array reallocation failure.\n");
+        exit(1);
+      }
+    }
+
+    /* Parse each line into structured data. */
+    parse_line_to_stand(stands + stands_count, data_line);
+    ++stands_count;
+  }
+
+  /* Close resources after reading. */
+  free(data_line);
+  if (fclose(input_handle) != 0) {
+    printf("Failed to close the input file, abort.\n");
+    exit(1);
+  }
+  printf("Successfully loaded %d rows from the input file.\n\n", stands_count);
+
+  printf("Processing all stand data...\n");
+
+  /* Open a temporary file for writing. */
+  FILE *output_handle = fopen(TEMP_OUTPUT_PATH, "w");
+  if (output_handle == NULL) {
+    printf("Failed to open the temp output file, abort.\n");
+    exit(1);
+  }
+
+  /* Write markdown header. */
+  fprintf(output_handle, "# JoJo's Stand Table\n\n");
+  /* Write the table header. */
+  fprintf(output_handle,
+          "| Stand | Power | Speed | Range | Persistence | Precision | "
+          "Potential | Total |\n");
+  fprintf(output_handle, "| --- | --- | --- | --- | --- | --- | --- | --- |\n");
+
+  /* Process each stand entity. */
+  int i = 0;
+  for (; i < stands_count; ++i) {
+    process_stand(stands + i, output_handle);
+  }
+
+  /* Close resources after writing. */
+  if (fclose(output_handle) != 0) {
+    printf("Failed to close the temp output file, abort.\n");
+    exit(1);
+  }
+  free(stands);
+
+  /* Rename the temp file. */
+  if (rename(TEMP_OUTPUT_PATH, OUTPUT_FILE_PATH) != 0) {
+    printf("Unable to rename the temp output file.\n");
+    printf("Please check and cleanup <%s>.\n", TEMP_OUTPUT_PATH);
+    exit(1);
+  }
+  printf("Successfully processed %d stand entities.\n\n", stands_count);
+
+  /* Print aggregate statistics. */
+  printf("=== Aggregate Statistics ===\n");
+  printf("Average power: %.2f\n",
+         (double)stand_aggregate_stats.total_power / stands_count);
+  printf("Average speed: %.2f\n",
+         (double)stand_aggregate_stats.total_speed / stands_count);
+  printf("Average range: %.2f\n",
+         (double)stand_aggregate_stats.total_range / stands_count);
+  printf("Average persistence: %.2f\n",
+         (double)stand_aggregate_stats.total_persistence / stands_count);
+  printf("Average precision: %.2f\n",
+         (double)stand_aggregate_stats.total_precision / stands_count);
+  printf("Average potential: %.2f\n",
+         (double)stand_aggregate_stats.total_potential / stands_count);
+  printf("Number of E or below: %d\n", stand_aggregate_stats.count_e_or_below);
+  printf("Number of D: %d\n", stand_aggregate_stats.count_d);
+  printf("Number of C: %d\n", stand_aggregate_stats.count_c);
+  printf("Number of B: %d\n", stand_aggregate_stats.count_b);
+  printf("Number of A or above: %d\n", stand_aggregate_stats.count_a_or_above);
+  printf("=== End ===\n\n");
+
+  printf("All done. Markdown file is created at %s.\n", OUTPUT_FILE_PATH);
+
+  return 0;
+}
+
+int read_stat_text_to_int_and_count(char *stat) {
+  int stat_score = 0;
+
+  if (stat == NULL) {
+    printf("Possibly missing stand stat in input data, abort.\n");
+    exit(1);
+  } else if (strcmp(stat, "None") == 0) {
+    stand_aggregate_stats.count_e_or_below += 1;
+  } else if (strcmp(stat, "E") == 0) {
+    stand_aggregate_stats.count_e_or_below += 1;
+    stat_score = 1;
+  } else if (strcmp(stat, "D") == 0) {
+    stand_aggregate_stats.count_d += 1;
+    stat_score = 2;
+  } else if (strcmp(stat, "C") == 0) {
+    stand_aggregate_stats.count_c += 1;
+    stat_score = 3;
+  } else if (strcmp(stat, "B") == 0) {
+    stand_aggregate_stats.count_b += 1;
+    stat_score = 4;
+  } else if (strcmp(stat, "A") == 0) {
+    stand_aggregate_stats.count_a_or_above += 1;
+    stat_score = 5;
+  } else if (strcmp(stat, "Infi") == 0) {
+    stand_aggregate_stats.count_a_or_above += 1;
+    stat_score = 6;
+  } else {
+    printf("Unknown stand stat <%s>, abort.\n", stat);
+    exit(1);
+  }
+
+  return stat_score;
+}
+
+void parse_line_to_stand(struct JoJoStand *stand, char *line) {
+  char *data_token;
+
+  /* Stand name. */
+  if ((data_token = strtok(line, CSV_DELIMITER)) != NULL) {
+    strncpy(stand->name, data_token, MAX_STAND_NAME_LEN + 1);
+  } else {
+    printf("Error parsing stand name from input data, abort.\n");
+    exit(1);
+  }
+
+  /* Stand stats. */
+  stand->power = read_stat_text_to_int_and_count(strtok(NULL, CSV_DELIMITER));
+  stand->speed = read_stat_text_to_int_and_count(strtok(NULL, CSV_DELIMITER));
+  stand->range = read_stat_text_to_int_and_count(strtok(NULL, CSV_DELIMITER));
+  stand->persistence =
+      read_stat_text_to_int_and_count(strtok(NULL, CSV_DELIMITER));
+  stand->precision =
+      read_stat_text_to_int_and_count(strtok(NULL, CSV_DELIMITER));
+  stand->potential =
+      read_stat_text_to_int_and_count(strtok(NULL, CSV_DELIMITER));
+}
+
+void write_stat_to_table(int stat, FILE *output_handle) {
+  if (stat == 0) {
+    fprintf(output_handle, " %s |", "None");
+  } else if (stat == 1) {
+    fprintf(output_handle, " %s |", "E");
+  } else if (stat == 2) {
+    fprintf(output_handle, " %s |", "D");
+  } else if (stat == 3) {
+    fprintf(output_handle, " %s |", "C");
+  } else if (stat == 4) {
+    fprintf(output_handle, " %s |", "B");
+  } else if (stat == 5) {
+    fprintf(output_handle, " %s |", "A");
+  } else if (stat == 6) {
+    fprintf(output_handle, " %s |", "Infinity");
+  }
+}
+
+void process_stand(struct JoJoStand *stand, FILE *output_handle) {
+  fprintf(output_handle, "| %s |", stand->name);
+
+  stand_aggregate_stats.total_power += stand->power;
+  write_stat_to_table(stand->power, output_handle);
+  stand_aggregate_stats.total_speed += stand->speed;
+  write_stat_to_table(stand->speed, output_handle);
+  stand_aggregate_stats.total_range += stand->range;
+  write_stat_to_table(stand->range, output_handle);
+  stand_aggregate_stats.total_persistence += stand->persistence;
+  write_stat_to_table(stand->persistence, output_handle);
+  stand_aggregate_stats.total_precision += stand->precision;
+  write_stat_to_table(stand->precision, output_handle);
+  stand_aggregate_stats.total_potential += stand->potential;
+  write_stat_to_table(stand->potential, output_handle);
+
+  fprintf(output_handle, " %d |\n",
+          stand->power + stand->speed + stand->range + stand->persistence +
+              stand->precision + stand->potential);
+}
