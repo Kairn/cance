12c7e4644e552b43b54350badcec8d5bbc70b0d4
Eddy Soma <esoma92.dev@gmail.com>
2023-12-25 13:13:19 -0500

    enemies init

--- src/13_shell_tanks.c
+++ src/13_shell_tanks.c
@@ -8,6 +8,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <termios.h>
+#include <time.h>
 #include <unistd.h>
 
 #define SCREEN_W 80
@@ -17,6 +18,15 @@ static int rendered = 0;
 
 #define FPS 10
 
+enum GameState {
+  INIT,
+  PLAYING,
+  WON,
+  LOST,
+  ENDED,
+};
+enum GameState G_STATE = INIT;
+
 static struct termios saved_term;
 
 #define IN_BUF_LEN 256
@@ -25,6 +35,10 @@ char IN_BUF[IN_BUF_LEN];
 #define BORDER_SIZE 3
 #define HF_LEN SCREEN_W *BORDER_SIZE
 #define BD_LEN SCREEN_W + (BORDER_SIZE - 1) * 4
+#define SCREEN_TOP 2
+#define SCREEN_BOTTOM 37
+#define SCREEN_LEFT 6
+#define SCREEN_RIGHT 81
 const char G_FRAME_H1[] = "╔╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╗";
 const char G_FRAME_H2[] = "╠╬╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╬╣";
 const char G_FRAME_BD[] = "╠╣                                                                            ╠╣";
@@ -59,6 +73,33 @@ const char *const G_FRAME_INS[] = {"╠╣                          MOVE: WASD, 
                                    "╠╣                          Press E to Continue                               ╠╣",
                                    "╠╣                                                                            ╠╣",
                                    "╠╣                          Press Q to Quit                                   ╠╣"};
+
+#define ENEMY_SIZE 10
+#define ENEMY_W 6
+#define ENEMY_H 3
+const char *const ENEMY_FRAME[] = {" ,--. ", "| oo |", "|/\\/\\|"};
+
+enum Direction {
+  STATIONARY,
+  UP,
+  DOWN,
+  LEFT,
+  RIGHT,
+};
+
+enum ObjectType {
+  TANK,
+  ENEMY,
+  BULLET,
+};
+
+struct Enemy {
+  enum ObjectType ot;
+  int row;
+  int col;
+  int life;
+  enum Direction dir;
+};
 
 /* Sets the terminal to non-canonical mode to read keyboard inputs. */
 void set_non_canonical_mode() {
@@ -120,7 +161,7 @@ void screen_fill_def() {
 }
 
 /* Runs the title screen with certain title text and waits for user input. */
-int run_title_screen(const char *const text[]) {
+void run_title_screen(const char *const text[]) {
   screen_fill_def();
   int i = 0;
   for (; i < SCREEN_H; ++i) {
@@ -151,13 +192,73 @@ int run_title_screen(const char *const text[]) {
     if (ui) {
       switch (ui) {
         case 'q':
-          return 0;
+          G_STATE = ENDED;
+          return;
         case 'e':
-          return 1;
+          G_STATE = PLAYING;
+          return;
         default:
           continue;
       }
     }
+    render();
+    frame_pause();
+  }
+}
+
+/* Randomly selects a new direction different from the current direction. */
+enum Direction get_rand_dir(enum Direction cur_dir) {
+  enum Direction dir = cur_dir;
+  while (dir == cur_dir) {
+    dir = rand() % 5;
+  }
+  return dir;
+}
+
+void draw_object(void *obj) {
+  enum ObjectType ot = *(enum ObjectType *)obj;
+  if (ot == ENEMY) {
+    int row = ((struct Enemy *)obj)->row;
+    int col = ((struct Enemy *)obj)->col;
+    int di = 0;
+    for (; di < ENEMY_H; ++di) {
+      memcpy(SCREEN[row + di] + col, ENEMY_FRAME[di], ENEMY_W);
+    }
+  } else if (ot == TANK) {
+    /* TODO. */
+  } else if (ot == BULLET) {
+    /* TODO. */
+  }
+}
+
+void run_game() {
+  struct Enemy enemies[ENEMY_SIZE];
+  int i = 0;
+  for (; i < ENEMY_SIZE; ++i) {
+    enemies[i].ot = ENEMY;
+    enemies[i].dir = STATIONARY;
+    enemies[i].life = 2;
+    enemies[i].col = SCREEN_LEFT + i * (ENEMY_W + 1);
+    enemies[i].row = SCREEN_TOP + rand() % 10;
+  }
+
+  while (1) {
+    switch (get_input_char()) {
+      case 'q':
+        G_STATE = ENDED;
+        return;
+      default:
+        break;
+    }
+
+    /* Logic to process objects. */
+
+    /* Draw all objects. */
+    screen_fill_def();
+    for (i = 0; i < ENEMY_SIZE; ++i) {
+      draw_object(&enemies[i]);
+    }
+
     render();
     frame_pause();
   }
@@ -168,6 +269,7 @@ int main(int argc, char const *argv[]) {
   tcgetattr(STDIN_FILENO, &saved_term);
   set_non_canonical_mode();
   /* TURN OFF STDOUT BUFFER: setbuf(stdout, NULL); */
+  srand(time(NULL));
 
   /* Initialize memory for the screen. */
   int i = 0;
@@ -175,16 +277,28 @@ int main(int argc, char const *argv[]) {
     SCREEN[i] = calloc(SCREEN_W + 1, sizeof(BORDER_SIZE));
   }
 
-  screen_fill_def();
-  render();
+  while (G_STATE != ENDED && G_STATE != PLAYING) {
+    screen_fill_def();
+    render();
+    switch (G_STATE) {
+      case WON:
+        run_title_screen(G_FRAME_WIN);
+        break;
+      case LOST:
+        run_title_screen(G_FRAME_LOSS);
+        break;
+      default:
+        run_title_screen(G_FRAME_TITLE);
+    }
 
-  int start = run_title_screen(G_FRAME_TITLE);
-  if (start) {
-    printf("Game starting...\n");
-  } else {
-    printf("Quitting...\n");
+    if (G_STATE == PLAYING) {
+      run_game();
+    } else {
+      break;
+    }
   }
 
+  printf("Quitting...\n");
   for (i = 0; i < SCREEN_H; ++i) {
     free(SCREEN[i]);
   }
